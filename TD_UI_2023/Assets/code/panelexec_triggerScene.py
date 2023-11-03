d3e82e7310e112800c86525f6944569270557ba4
Jive-Faury <jiveofmars@gmail.com>
2023-11-03 06:22:43 +0100

    Update panelexec_triggerScene.py

--- TD_UI_2023/Assets/code/panelexec_triggerScene.py
+++ TD_UI_2023/Assets/code/panelexec_triggerScene.py
@@ -4,26 +4,28 @@
 # Make sure the corresponding toggle is enabled in the Panel Execute DAT.
 
 def onOffToOn(panelValue):
-	s1= op.Engine.op('render1')
-	s2= op.Engine.op('render2')
-	
+	s1 = op.Engine.op('render1')
+	s2 = op.Engine.op('render2')
+	s3 = op.Engine.op('selectScene1')
+	s4 = op.Engine.op('selectScene2')
+
 	trans = op.Engine.op('trans')
 	change = op.Engine.op('change')['chan1']
 	speed = op.Engine.op('speed')[0]
 
 	
 	target1 = op.Scenes.op('scene'  +str(me.parent().digits)  +'/content/geo*')
-	#target2 = op.Engine.op('pathLights')[parent().digits,0]
+	target2 = op.Scenes.op('scene'  +str(me.parent().digits)  +'/content/texture')
 	
 	
 	if change == 0:
 		s2.par.geometry = target1
-		#s2.par.lights= target2
+		s4.par.top = target2
 		trans.par.value0 = speed
 
 	else:
 		s1.par.geometry = target1
-		#s1.par.lights= target2
+		s3.par.top= target2
 		trans.par.value0 = -speed
 	return
 
